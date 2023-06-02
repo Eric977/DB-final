77f6f41967e47389a35fa563d321043b3ab8ed75
ACaccel <c8763318@gmail.com>
2023-06-02 23:16:17 +0800

    add preinstall.sh

---
+++ scripts/preinstall.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+mkdir ../compressions
+cd ../compressions
+
+# Install lz4
+echo -e "\n----------Installing lz4----------\n"
+git clone https://github.com/lz4/lz4.git
+cd lz4
+make
+make install
+cd ..
+echo -e "\n----------Finished installing lz4----------\n"
+
+# Install snappy
+echo -e "\n----------Installing snappy----------\n"
+git clone https://github.com/google/snappy.git
+cd snappy
+git submodule update --init
+mkdir build
+cd build && cmake ../ && make
+cd ../..
+echo -e "\n----------Finished installing snappy----------\n"
+
+# Install FastPFor
+echo -e "\n----------Installing FastPFor----------\n"
+git clone https://github.com/lemire/FastPFor.git
+cd FastPFor
+mkdir build
+cd build
+cmake ..
+cmake --build .
+cd ../..
+echo -e "\n----------Finished installing FastPFor----------\n"
