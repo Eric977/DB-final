0a767b5f50ab21e9e1f508725761a5d16038698a
ACaccel <c8763318@gmail.com>
2023-06-09 14:05:57 +0800

    add fastpfor compression

--- btree/include/encode.hpp
+++ btree/include/encode.hpp
@@ -1,15 +1,13 @@
 #include <vector>
 #include <lz4.h>
 #include <snappy.h>
-
-enum CompressType {
-    LZ4,
-    SNAPPY
-};
+#include <codecfactory.h>
+#include <deltautil.h>
 
 // data to char array
 std::vector<char> data_to_char_array(std::vector<std::pair<std::string, std::string>>& data) {
     std::vector<char> formatted_data;
+    
     for (const auto& val : data) {
         formatted_data.insert(
             formatted_data.end(),
@@ -24,6 +22,7 @@ std::vector<char> data_to_char_array(std::vector<std::pair<std::string, std::str
         );
         formatted_data.push_back('\0');
     }
+    
     return formatted_data;
 }
 
@@ -31,6 +30,7 @@ std::vector<char> data_to_char_array(std::vector<std::pair<std::string, std::str
 std::vector<std::pair<std::string, std::string>> char_array_to_data(std::vector<char>& formatted_decompressed, int vectorSize) {
     std::vector<std::pair<std::string, std::string>> decompressed;
     int index = 0;
+    
     for (int i = 0; i < vectorSize; ++i) {
         std::string first(formatted_decompressed.data() + index);
         index += first.size() + 1;
@@ -38,7 +38,60 @@ std::vector<std::pair<std::string, std::string>> char_array_to_data(std::vector<
         index += second.size() + 1;
         decompressed.emplace_back(std::move(first), std::move(second));
     }
+
     return decompressed;
+}
+
+// convert vector string to uint32_t
+void strToUint32(std::vector<uint32_t> &vstr, std::vector<std::string> strs, int strSize){
+    for (std::string str : strs) {       
+        for (int i = 0; i < strSize; i += 4){
+            uint32_t tmp = 0;
+            for (int j = 0; j < 4; j ++){
+                tmp = tmp << 8;
+                if (i + j < str.size())
+                    tmp += str[i + j];
+                else
+                    tmp += 0;
+            }
+            vstr.push_back(tmp);
+        }
+    }
+}
+
+// convert vector uint32_t to string
+void uint32ToStr(std::vector<uint32_t> vstr, std::vector<std::string> &str, int strSize){
+    int strLen = strSize/4;
+    
+    for (int i = 0; i < vstr.size(); i += strLen) {
+        std::vector<uint32_t> tmpVec(vstr.begin() + i, vstr.begin() + i + strLen);
+        std::string tmpStr;
+
+        for (int i = 0; i < tmpVec.size(); i++){
+            for (int j = 0; j < 4; j ++){
+                tmpStr += (char)(tmpVec[i] >> (24 - j * 8));
+            }
+        }
+
+        str.push_back(tmpStr);
+    }
+}
+
+// convert vector<pair<T, T>> to two vector
+template<typename T>
+void pairToTwoVec(std::vector<std::pair<T, T>> &vstr, std::vector<T> &v1, std::vector<T> &v2){
+    for (int i = 0; i < vstr.size(); i ++){
+        v1.push_back(vstr[i].first);
+        v2.push_back(vstr[i].second);
+    }
+}
+
+// convert two vector to vector<pair<T, T>>
+template<typename T>
+void twoVecToPair(std::vector<std::pair<T, T>> &vstr, std::vector<T> v1, std::vector<T> v2){
+    for (int i = 0; i < v1.size(); i ++){
+        vstr.push_back(std::make_pair(v1[i], v2[i]));
+    }
 }
 
 template<typename T>
@@ -131,25 +184,38 @@ std::vector<T> snappy_decompress(std::vector<char>& compressed, int originalSize
 }
 
 template<typename T>
-std::vector<char> compress(CompressType compressType, std::vector<T>& data, int &srcSize) {
-    switch (compressType) {
-        case LZ4:
-            return lz4_compress<T>(data, srcSize);
-        case SNAPPY:
-            return snappy_compress<T>(data);
-        default:
-            return lz4_compress<T>(data, srcSize);
+std::vector<std::pair<uint32_t, uint32_t>> fastpfor_compress(std::vector<T> &data, int &srcSize){
+    if (std::is_same<T, std::pair<std::string, std::string>>::value) {
+        std::vector<std::pair<uint32_t, uint32_t>> uint_data;
+        std::vector<uint32_t> uint_keys, uint_values;
+        std::vector<std::string> keys, values;
+
+        pairToTwoVec<std::string>(data, keys, values);
+
+        strToUint32(uint_keys, keys, srcSize);
+        strToUint32(uint_values, values, srcSize);
+
+        twoVecToPair<uint32_t>(uint_data, uint_keys, uint_values);
+
+        return uint_data;
+    }
+    else {
+        throw std::invalid_argument("fastpfor_compress: unsupported type");
     }
 }
 
 template<typename T>
-std::vector<T> decompress(CompressType compressType, std::vector<char>& compressed, int originalSize, int vectorSize) {
-    switch (compressType) {
-        case LZ4:
-            return lz4_decompress<T>(compressed, originalSize, vectorSize);
-        case SNAPPY:
-            return snappy_decompress<T>(compressed, originalSize, vectorSize);
-        default:
-            return lz4_decompress<T>(compressed, originalSize, vectorSize);
-    }
+std::vector<T> fastpfor_decompress(std::vector<std::pair<uint32_t, uint32_t>> &uint_data, int srcSize){
+    std::vector<uint32_t> uint_keys, uint_values;
+    std::vector<std::string> keys, values;
+    std::vector<T> data;
+
+    pairToTwoVec<uint32_t>(uint_data, uint_keys, uint_values);
+
+    uint32ToStr(uint_keys, keys, srcSize);
+    uint32ToStr(uint_values, values, srcSize);
+
+    twoVecToPair<std::string>(data, keys, values);
+
+    return data;
 }
--- btree/src/main.cpp
+++ btree/src/main.cpp
@@ -8,12 +8,16 @@
 #include <thread>
 #include <fstream>
 
+#include "../include/encode.hpp"
 #include "../include/btree_map.hpp"
 
 using namespace std;
 
 void loadKVpair(string file, vector<string>& keys, vector<string>& values){
     std::ifstream inputFile(file);  
+}
+
+
 
 int main(){
     std::this_thread::sleep_for(std::chrono::seconds(1));
@@ -35,4 +39,64 @@ int main(){
     // cout << "avgfill = " << bt->get_stats().avgfill_leaves() << std::endl;
     // cout << "mem = " << (256 * bt->get_stats().nodes()) << std::endl;
 
+
+
+    ////////////////////////////////////////////////////////////////////
+    pair<string, string> data[20] = {
+        {"identifier1", "alpha@gamil.com"},
+        {"identifier2", "bravo@gamil.com"},
+        {"identifier3", "charlie@gmail.com"},
+        {"identifier4", "delta@gmail.com"},
+        {"identifier5", "echo@gmail.com"},
+        {"identifier6", "foxtrot@gmail.com"},
+        {"identifier7", "golf@ntu.edu.tw"},
+        {"identifier8", "hotel@ntu.edu.tw"},
+        {"identifier9", "india@ntu.edu.tw"},
+        {"identifier10", "juliett@microsoft.com"},
+        {"identifier11", "kilo@microsoft.com"},
+        {"identifier12", "lima@microsoft.com"},
+        {"identifier13", "mike@microsoft.com"},
+        {"identifier14", "november@gmail.com"},
+        {"identifier15", "oscar@gmail.com"},
+        {"identifier16", "papa@fatcat.com"},
+        {"identifier17", "quebec@fatcat.com"},
+        {"identifier18", "romeo@fatcat.com"},
+        {"identifier19", "sierra@fatcat.com"},
+        {"identifier20", "tango@fatcat.com"}
+    };
+    vector<pair<string, string>> original;
+    for (int i = 0; i < 20; i ++) {
+        original.push_back(data[i]);
+    }
+
+    // lz4 compression
+    int lz4_srcSize = 0;
+    vector<char> lz4_compressed = lz4_compress<pair<string, string>>(original, lz4_srcSize);
+    vector<pair<string, string>> lz4_decompressed = lz4_decompress<pair<string, string>>(lz4_compressed, lz4_srcSize, original.size());
+    for (const auto& val : lz4_decompressed) {
+        std::cout << val.first << " " << val.second << " " << std::endl;
+    }
+    cout << endl;
+
+    // snappy compression
+    int snappy_srcSize = 0;
+    vector<char> snappy_compressed = snappy_compress<pair<string, string>>(original);
+    vector<pair<string, string>> snappy_decompressed = snappy_decompress<pair<string, string>>(snappy_compressed, snappy_srcSize, original.size());
+    for (const auto& val : snappy_decompressed) {
+        std::cout << val.first << " " << val.second << " " << std::endl;
+    }
+    cout << endl;
+
+    // fastpfor compression
+    int fastpfor_srcSize = 24;  // should be multiple of 4
+    vector<pair<uint32_t, uint32_t>> fastpfor_compressed = fastpfor_compress<pair<string, string>>(original, fastpfor_srcSize);
+    vector<pair<string, string>> fastpfor_decompressed = fastpfor_decompress<pair<string, string>>(fastpfor_compressed, fastpfor_srcSize);
+    for (const auto& val : fastpfor_decompressed) {
+        std::cout << val.first << " " << val.second << " " << std::endl;
+    }
+    cout << endl;
+
+    ////////////////////////////////////////////////////////////////////
+
+    return 0;
 }
